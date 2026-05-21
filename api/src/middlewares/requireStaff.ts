95bb7843ff18ac7c5b3f547780f0c4b55a3d3651
Hippolyte <hippolyte.devweb@gmail.com>
2026-05-21 14:13:56 +0200

    feat : add middlewares requireStaff for secure routes

---
+++ api/src/middlewares/requireStaff.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { auth } from "../auth.js";
+import { myPrisma } from "../prisma.js";
+
+export const requireStaff = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const session = await auth.api.getSession();
+
+  if (!session?.user.id) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const staffProfiles = await myPrisma.staffProfile.findUnique({
+    where: { userId: session.user.id, deletedAt: null },
+    select: { id: true },
+  });
+
+  if (!staffProfiles) {
+    return res.status(403).json({ error: "Forbidden" });
+  }
+
+  next();
+};
