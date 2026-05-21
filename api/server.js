010ffbb7f472e8d712321b59fdc79994f11053b1
Hippolyte <hippolyte.devweb@gmail.com>
2026-05-21 10:46:00 +0200

    chore : add minimal server.js logic for /health to work

--- api/server.js
+++ api/server.js
@@ -1,22 +1,25 @@
 import express from "express";
 import "dotenv/config";
 import { toNodeHandler, fromNodeHeaders } from "better-auth/node";
-import { auth } from "./auth";
+import cors from "cors";
 
 const app = express();
 const port = 3005;
 
-app.all("/api/auth/*", toNodeHandler(auth)); // For ExpressJS v4
-// app.all("/api/auth/*splat", toNodeHandler(auth)); For ExpressJS v5
-
 // Mount express json middleware after Better Auth handler
 // or only apply it to routes that don't interact with Better Auth
 app.use(express.json());
+
+// Faire un health plus complets plus tard //
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // Replace with your frontend's origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   }),
 );
 
