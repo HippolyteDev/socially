c5f695bc4b3e383ecca5298d2039807cf4d73963
Hippolyte <hippolyte.devweb@gmail.com>
2026-05-21 11:05:50 +0200

    chore : add prisma to api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import { toNodeHandler, fromNodeHeaders } from "better-auth/node";
+import { myPrisma } from "./prisma.js";
 import cors from "cors";
 
 const app = express();
@@ -11,8 +12,9 @@ const port = 3005;
 app.use(express.json());
 
 // Faire un health plus complets plus tard //
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
+app.get("/health", async (req, res) => {
+  const usersCount = await myPrisma.user.count();
+  res.status(200).json({ status: "ok", usersCount });
 });
 
 app.use(
