79fce0b920724163b8bbcf5af699f84feec2f2b0
Hippolyte <hippolyte.devweb@gmail.com>
2026-05-22 18:42:41 +0200

    feat: add rbac logique on requireStaff

--- api/src/middlewares/requireStaff.ts
+++ api/src/middlewares/requireStaff.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { auth } from "../auth.js";
 import { myPrisma } from "../prisma.js";
 import { StaffRequest } from "../routes/staff.js";
+import { StaffRole } from "@prisma/client";
 
 export const requireStaff = async (
   req: Request,
@@ -22,10 +23,24 @@ export const requireStaff = async (
   });
 
   if (!staffProfile) {
-    return res.status(403).json({ error: "Forbidden", staffProfile });
+    return res.status(403).json({ error: "Forbidden" });
   }
 
   (req as StaffRequest).staff = staffProfile;
 
   next();
+};
+
+export const requireStaffRole = (allowedRoles: StaffRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    await requireStaff(req, res, () => {
+      const staffReq = req as StaffRequest;
+
+      if (!allowedRoles.includes(staffReq.staff.role)) {
+        return res.status(403).json({ error: "Forbidden" });
+      }
+
+      return next();
+    });
+  };
 };
